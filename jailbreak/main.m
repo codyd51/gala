d1c2fa9bfb0bcd76c1a1aa666be193a47fb6fc7f
Phillip Tennen <pt@datatheorem.io>
2023-07-08 14:04:27 +0100

    Initial Commit

---
+++ jailbreak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jailbreak
+//
+//  Created by Phillip Tennen on 08/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
