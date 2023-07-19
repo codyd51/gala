f13ac22f188b5938980a39747d9ac0f1b0cb90f7
Phillip Tennen <pt@datatheorem.io>
2023-07-19 21:19:33 +0100

    Add a container to logically group sets of patches

--- patcher.py
+++ patcher.py
@@ -145,6 +145,21 @@ class BlobPatch(Patch):
         image_data[patch_file_offset:patch_file_offset + len(self.new_content)] = self.new_content
 
 
+@dataclass
+class PatchSet(Patch):
+    """A collection of patches that are logically grouped together.
+    This has no difference in functionality to declaring top-level patches individually, and serves purely as an
+    organizational tool.
+    """
+    name: str
+    patches: list[Patch]
+
+    def apply(self, image_base_address: VirtualMemoryPointer, image_data: bytearray) -> None:
+        print(f'Applying patch set {self.name}...')
+        for patch in self.patches:
+            patch.apply(image_base_address, image_data)
+
+
 class FunctionRepository:
     _BUILDS_TO_KNOWN_FUNCTIONS = TotalEnumMapping({
         OsBuildEnum.iPhone3_1_4_0_8A293: [],
@@ -179,62 +194,75 @@ class PatchRepository:
         # PT: This needs to be a method, rather than a class variable, because otherwise it
         # captures file data **when the class is defined/interpreted**,
         # which is before we've rebuilt the shellcode image with new code! Annoying
-        shellcode_addr = 0x840000fc
         return TotalEnumMapping({
             OsBuildEnum.iPhone3_1_4_0_8A293: _binary_types_mapping({
                 ImageType.iBSS: [
-                    # More logging patches, #2 and #3 might not be needed?
-                    InstructionPatch(
-                        # Load memory to find the value that should be passed to debug_enable_uarts()
-                        # We always want debug logs, so override the value here
-                        function_name="platform_early_init",
-                        address=VirtualMemoryPointer(0x84010b96),
-                        orig_instructions=[Instr.thumb("ldrb r0, [r4]")],
-                        patched_instructions=[Instr.thumb("movs r0, #3")],
+                    PatchSet(
+                        name="Enable UART debug logs",
+                        patches=[
+                            # More logging patches, #2 and #3 might not be needed?
+                            InstructionPatch(
+                                # Load memory to find the value that should be passed to debug_enable_uarts()
+                                # We always want debug logs, so override the value here
+                                function_name="platform_early_init",
+                                address=VirtualMemoryPointer(0x84010b96),
+                                orig_instructions=[Instr.thumb("ldrb r0, [r4]")],
+                                patched_instructions=[Instr.thumb("movs r0, #3")],
+                            ),
+                            # More UART patch
+                            InstructionPatch(
+                                function_name="maybe_iBSS_start",
+                                address=VirtualMemoryPointer(0x84000994),
+                                orig_instructions=[Instr.arm("bl #0x840153f4")],
+                                patched_instructions=[Instr.thumb("movs r0, #3"), Instr.thumb("nop")],
+                            ),
+                            # More UART patch
+                            InstructionPatch(
+                                function_name="maybe_iBSS_start",
+                                address=VirtualMemoryPointer(0x84000838),
+                                orig_instructions=[Instr.arm("bl #0x840153f4")],
+                                patched_instructions=[Instr.thumb("movs r0, #3"), Instr.thumb("nop")],
+                            ),
+                        ]
                     ),
-                    # More UART patch
-                    InstructionPatch(
-                        function_name="maybe_iBSS_start",
-                        address=VirtualMemoryPointer(0x84000994),
-                        orig_instructions=[Instr.arm("bl #0x840153f4")],
-                        patched_instructions=[Instr.thumb("movs r0, #3"), Instr.thumb("nop")],
+                    PatchSet(
+                        name="Load unsigned iBEC",
+                        patches=[
+                            # iBEC loading patches
+                            # Check PROD tag on image3
+                            InstructionPatch(
+                                function_name="",
+                                address=VirtualMemoryPointer(0x8400df14),
+                                orig_instructions=[Instr.thumb("cmp r0, #0")],
+                                patched_instructions=[Instr.thumb("cmp r0, r0")],
+                            ),
+                            # Check ECID tag on image3
+                            InstructionPatch(
+                                function_name="",
+                                address=VirtualMemoryPointer(0x8400e00c),
+                                orig_instructions=[Instr.thumb("cbz r0, #0x8400e02e")],
+                                patched_instructions=[Instr.thumb("b #0x8400e02e")],
+                            ),
+                        ]
                     ),
-                    # More UART patch
-                    InstructionPatch(
-                        function_name="maybe_iBSS_start",
-                        address=VirtualMemoryPointer(0x84000838),
-                        orig_instructions=[Instr.arm("bl #0x840153f4")],
-                        patched_instructions=[Instr.thumb("movs r0, #3"), Instr.thumb("nop")],
+                    PatchSet(
+                        name="Custom picture loading patches",
+                        patches=[
+                            # Custom boot logo patches
+                            InstructionPatch.quick(0x8400def0, Instr.thumb("movs r1, #0")),
+                            # We patch the comparison, so hard-code the branch direction
+                            InstructionPatch.quick(0x8400df10, [Instr.thumb("movs r0, #0"), Instr.thumb("nop")], expected_length=4),
+
+                            # Match the registers for a success call
+                            InstructionPatch.quick(0x8400df5a, [Instr.thumb("movs r0, #0"), Instr.thumb("mov r1, r2"), Instr.thumb("movs r2, #1")], expected_length=6),
+
+                            # After the call to validate_shsh_and_cert, `r0 == 0` to indicate success. If successful,
+                            # we branch away. We always should take the branch.
+                            InstructionPatch.quick(0x8400de98, Instr.thumb("b #0x8400e1e8")),
+
+                            InstructionPatch.quick(0x8400e0a6, Instr.thumb("cmp r3, r3")),
+                        ],
                     ),
-
-                    # iBEC loading patches
-                    # Check PROD tag on image3
-                    InstructionPatch(
-                        function_name="",
-                        address=VirtualMemoryPointer(0x8400df14),
-                        orig_instructions=[Instr.thumb("cmp r0, #0")],
-                        patched_instructions=[Instr.thumb("cmp r0, r0")],
-                    ),
-                    # Check ECID tag on image3
-                    InstructionPatch(
-                        function_name="",
-                        address=VirtualMemoryPointer(0x8400e00c),
-                        orig_instructions=[Instr.thumb("cbz r0, #0x8400e02e")],
-                        patched_instructions=[Instr.thumb("b #0x8400e02e")],
-                    ),
-
-                    InstructionPatch.quick(0x8400def0, Instr.thumb("movs r1, #0")),
-                    # We patch the comparison, so hard-code the branch direction
-                    InstructionPatch.quick(0x8400df10, [Instr.thumb("movs r0, #0"), Instr.thumb("nop")], expected_length=4),
-
-                    # Match the registers for a success call
-                    InstructionPatch.quick(0x8400df5a, [Instr.thumb("movs r0, #0"), Instr.thumb("mov r1, r2"), Instr.thumb("movs r2, #1")], expected_length=6),
-
-                    # After the call to validate_shsh_and_cert, `r0 == 0` to indicate success. If successful,
-                    # we branch away. We always should take the branch.
-                    InstructionPatch.quick(0x8400de98, Instr.thumb("b #0x8400e1e8")),
-
-                    InstructionPatch.quick(0x8400e0a6, Instr.thumb("cmp r3, r3")),
                 ],
                 ImageType.iBEC: [],
             }),
