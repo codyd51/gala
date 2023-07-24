f55e45c7b7fb49ff9e90d2ca2a386b1ca5b1e73b
Phillip Tennen <pt@datatheorem.io>
2023-07-24 17:50:53 +0100

    Check in main runner

--- jailbreak.py
+++ jailbreak.py
@@ -1,10 +1,12 @@
+import shutil
 import subprocess
+import tempfile
 import time
 from contextlib import contextmanager
 from dataclasses import dataclass
 from enum import Enum, auto
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Mapping
 from collections.abc import Iterator
 
 import usb
@@ -13,9 +15,9 @@ from usb.backend import libusb1
 from usb.backend.libusb1 import _LibUSB
 
 from os_build import OsBuildEnum, ImageType
-from patcher import regenerate_patched_images, IpswPatcherConfig
+from patcher import regenerate_patched_images, IpswPatcherConfig, generate_patched_ipsw
 from recompile_payloads import recompile_payloads
-from utils import TotalEnumMapping, chunks
+from utils import TotalEnumMapping, chunks, run_and_check
 
 
 def get_libusb_backend() -> _LibUSB:
@@ -182,6 +184,8 @@ def exploit_and_upload_image(image_path: Path):
 
 
 def main():
+    #dump_usb_devices()
+    #return
     patcher_config = IpswPatcherConfig(
         OsBuildEnum.iPhone3_1_4_0_8A293,
         replacement_pictures={
@@ -191,6 +195,7 @@ def main():
     # We need to always recompile the payloads because they may impact what gets injected into the patched images
     recompile_payloads()
     image_types_to_paths = regenerate_patched_images(patcher_config)
+    generate_patched_ipsw(patcher_config.os_build, image_types_to_paths)
 
     # Wait for a DFU device to connect
     print('Awaiting DFU device...')
@@ -198,6 +203,7 @@ def main():
         print(f'Got DFU device')
 
     exploit_and_upload_image(image_types_to_paths[ImageType.iBSS])
+    time.sleep(2)
 
     # The exploit payload will load and jump to the iBSS, which will present as a Recovery Mode device
     with acquire_device(DeviceMode.Recovery) as recovery_device:
@@ -229,12 +235,35 @@ def main():
         recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/Firmware/all_flash/all_flash.n90ap.production/DeviceTree.n90ap.img3"))
         recovery_device.send_command("devicetree")
         time.sleep(2)
-        recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6306-403.dmg"))
+        # recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6306-403.dmg"))
+        recovery_device.upload_file(image_types_to_paths[ImageType.RestoreRamdisk])
+        #recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6305-405.dmg"))
         #recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6303-385.dmg"))
         recovery_device.send_command("ramdisk")
         time.sleep(2)
         recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/kernelcache.release.n90"))
-        recovery_device.send_command("bootx")
+        # PT: Should replace with below, just didn't want to ry 2 things tat once
+        #recovery_device.upload_file(image_types_to_paths[ImageType.KernelCache])
+
+        try:
+            recovery_device.send_command("bootx")
+        except usb.core.USBError:
+            # The device may drop the connection when jumping to the kernel, but it's no problem
+            pass
+
+    time.sleep(5)
+
+    # Start the restore process with the modified IPSW
+    # TODO(PT): Repack into an ipsw as below
+    if False:
+        run_and_check([
+            "/Users/philliptennen/Documents/Jailbreak/tools/idevicerestore/src/idevicerestore",
+            "--restore-mode",
+            "/Users/philliptennen/Documents/Jailbreak/patched_images/iPhone3,1_4.0_8A293/patched.ipsw",
+        ])
+    # ./xpwntool /Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6306-403.dmg decrypted_ramdisk -k 62aabe3e763eb3669b4922468be2acb787199c6b0ef8ae873c312e458d9b9be3 -iv 0ab135879934fdd0d689b3d0f8cf8374
+
+    # dump_usb_devices()
 
 
 if __name__ == '__main__':
