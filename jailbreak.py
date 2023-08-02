00cfa9e626b310396fe45630d288ac5912d97e38
Phillip Tennen <pt@datatheorem.io>
2023-08-02 21:44:15 +0100

    Tweak timings in main runner

--- jailbreak.py
+++ jailbreak.py
@@ -24,12 +24,14 @@ def main():
     # We need to always recompile the payloads because they may impact what gets injected into the patched images
     recompile_payloads()
     image_types_to_paths = regenerate_patched_images(patcher_config)
-    generate_patched_ipsw(patcher_config.os_build, image_types_to_paths)
+    if False:
+        generate_patched_ipsw(patcher_config.os_build, image_types_to_paths)
 
     # Run our payload in SecureROM on a connected DFU device
     securerom_shellcode_path = Path(__file__).parent / "payload_stage1" / "build" / "payload_stage1_shellcode"
     securerom_shellcode = securerom_shellcode_path.read_bytes()
     print(f'SecureROM shellcode length: {len(securerom_shellcode)}')
+
     execute_securerom_payload(securerom_shellcode)
 
     # Give the on-device payload a chance to run
@@ -47,9 +49,10 @@ def main():
     # Call this just for the side effect of waiting until the Recovery Mode device pops up
     # If it does, the iBSS has successfully launched
     acquire_device_with_timeout(DeviceMode.Recovery)
+    time.sleep(1)
 
     # The exploit payload will load and jump to the iBSS, which will present as a Recovery Mode device
-    with acquire_device(DeviceMode.Recovery) as recovery_device:
+    with acquire_device_with_timeout(DeviceMode.Recovery) as recovery_device:
         # Upload and set the boot logo
         recovery_device.upload_file(image_types_to_paths[ImageType.AppleLogo])
         recovery_device.send_command("setpicture")
@@ -81,9 +84,9 @@ def main():
         recovery_device.upload_file(image_types_to_paths[ImageType.RestoreRamdisk])
         recovery_device.send_command("ramdisk")
         time.sleep(2)
-        recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/kernelcache.release.n90"))
-        # PT: Should replace with below, just didn't want to ry 2 things tat once
-        #recovery_device.upload_file(image_types_to_paths[ImageType.KernelCache])
+        #recovery_device.upload_file(Path("/Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/kernelcache.release.n90"))
+        # PT: Should replace with below, just didn't want to ry 2 things at once
+        recovery_device.upload_file(image_types_to_paths[ImageType.KernelCache])
 
         try:
             recovery_device.send_command("bootx")
@@ -95,11 +98,13 @@ def main():
 
     # Start the restore process with the modified IPSW
     # TODO(PT): Repack into an ipsw as below
+
     if False:
         run_and_check([
             "/Users/philliptennen/Documents/Jailbreak/tools/idevicerestore/src/idevicerestore",
             "--restore-mode",
-            "/Users/philliptennen/Documents/Jailbreak/patched_images/iPhone3,1_4.0_8A293/patched.ipsw",
+            #"/Users/philliptennen/Documents/Jailbreak/patched_images/iPhone3,1_4.0_8A293/patched.ipsw",
+            "/Users/philliptennen/Documents/Jailbreak/orig_ipsw/iPhone3,1_4.0_8A293_Restore.ipsw",
         ])
     # ./xpwntool /Users/philliptennen/Documents/Jailbreak/ipsw/iPhone3,1_4.0_8A293_Restore.ipsw.unzipped/018-6306-403.dmg decrypted_ramdisk -k 62aabe3e763eb3669b4922468be2acb787199c6b0ef8ae873c312e458d9b9be3 -iv 0ab135879934fdd0d689b3d0f8cf8374
 
