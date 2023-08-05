8c961189621b9a52e19af55b88c9e30184d4e44d
Phillip Tennen <pt@datatheorem.io>
2023-08-05 10:15:42 +0100

    Check in current iBSS shellcode

--- shellcode_within_ibss/compile.sh
+++ shellcode_within_ibss/compile.sh
@@ -6,6 +6,7 @@
 #  Created by Phillip Tennen on 10/07/2023.
 #  
 as -arch armv7 entry.s -o build/entry.o
-gcc -c -arch armv7 body.c -ffreestanding -o build/body.o -mthumb -fpic
-ld build/entry.o build/body.o -U _main -U start -static -o build/shellcode_within_ibss_packed
+#gcc -c -arch armv7 body.c -ffreestanding -o build/body.o -mthumb -fpic
+#ld build/entry.o build/body.o -U _main -U start -static -o build/shellcode_within_ibss_packed
+ld build/entry.o -U _main -U start -static -o build/shellcode_within_ibss_packed
 python3 ../dump_shellcode.py build/shellcode_within_ibss_packed build/shellcode_within_ibss_shellcode
--- shellcode_within_ibss/entry.s
+++ shellcode_within_ibss/entry.s
@@ -3,43 +3,34 @@
 .extern _c_entry_point
 
 .pool
-.set dprintf, 0x84016fc9
+@.set printf, 0x5ff12d90
+.set printf, 0x5ff16940
 
 .code 16
 _start: .global _start
     push {lr}
 
     @ Preserve registers
-    push {r0-r11}
+    @push {r0-r11}
 
-    @mov r0, #0
-    @mov r1, #1
-    @mov r2, #2
-    @mov r3, #3
-    @mov r4, #4
-    @mov r5, #5
-    @mov r6, #6
-    @mov r7, #7
-    @mov r8, #8
-    @mov r9, #9
-    @mov r10, #10
+    @push {r6}
+    @push {r7}
+    @push {r8}
 
-    push {r6}
-    push {r7}
-    push {r8}
+    @push {r3}
+    @push {r4}
+    @push {r5}
 
-    push {r3}
-    push {r4}
-    push {r5}
-
-    push {r0}
-    push {r1}
-    push {r2}
+    @push {r0}
+    @push {r1}
+    @push {r2}
 
     adr r0, intro
-    movw r11, #0x6fc9
-    movt r11, #0x8401
-    blx r11
+    mov r1, lr
+    ldr r8, =printf
+    blx r8
+
+    pop {pc}
 
     pop {r3}
     pop {r2}
@@ -53,14 +44,14 @@ _start: .global _start
     pop {r1}
 
     adr r0, msg1
-    blx r11
+    blx r10
 
     pop {r3}
     pop {r2}
     pop {r1}
 
     adr r0, msg1
-    blx r11
+    blx r10
 
     @ Restore registers
     pop {r0-r11}
@@ -68,21 +59,8 @@ _start: .global _start
     pop {pc}
 
 .align 2
-msg:
-.asciz "test\n\0"
-@.asciz "image_load_memory(%08x, %08x, %08x, %08x, %08x, %08x, %08x)\n\0"
-@.asciz "We get to image3_load_copyobject(r0=0x%x, r1=?, r2=0x%08x, r3=0x%08x, r4=0x%08x, r5=0x%08x)\n\0"
-@.asciz "Inspect r0=0x%x, r1=?, r2=0x%08x, r3=0x%08x, r4=0x%08x, r5=0x%08x\n\0"
-@.asciz "PWNED jumping into image at 0x%08x!.\n\0"
-@.asciz "The above block doesn't jump!\n\0"
-@.asciz "r0 = 0x%08x\nr1 = ?\nr2 = 0x%08x\nr3 = 0x%08x\nr4 = 0x%08x\nr5 = 0x%08x\nr6 = 0x%08x\n\0"
-@msg_len = . - msg
-
 intro:
-.asciz "\nRegister dump\n\0"
+.asciz "\nRegdump@ 0x%08x\n\0"
 
 msg1:
-@.asciz "r0 = 0x%08x\nr1 = 0x%08x\nr2 = 0x%08x\nr3 = 0x%08x\nr4 = 0x%08x\nr5 = 0x%08x\nr6 = 0x%08x\nr7 = 0x%08x\nr8 = 0x%08x\nr9 = 0x%08x\nr10 = 0x%08x\n\0"
 .asciz "0x%08x\n0x%08x\n0x%08x\n\0"
-@msg2:
-@.asciz "msg2 r2 = 0x%08x\nmsg2 r3 = 0x%08x\nmsg2 r4 = 0x%08x\nmsg2 r5 = 0x%08x\nmsg2 r6 = 0x%08x\n\n\0"
