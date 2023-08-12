366225f0b109b355ceef6d15591191b50bc6087f
Phillip Tennen <pt@datatheorem.io>
2023-08-12 08:16:52 +0100

    Output is buffered and periodically flushed to logs view

--- runner_app/main.m
+++ runner_app/main.m
@@ -9,12 +9,63 @@
 }
 @end
 
+@interface LogsView : NSViewWithTopLeftCoordinateSystem
+@property (retain) NSTextView* textView;
+@end
+
+@implementation LogsView
+- (BOOL)isFlipped {
+    return YES;
+}
+
+- (void)append:(NSString*)text {
+    if (!text.length) {
+        return;
+    }
+    NSDictionary *attrs = @{
+            NSForegroundColorAttributeName: [NSColor colorWithRed:0.4 green:1.0 blue:0.4 alpha:1.0],
+            NSFontAttributeName: [NSFont monospacedSystemFontOfSize:14 weight:NSFontWeightMedium]
+    };
+    NSAttributedString* attr = [[NSAttributedString alloc] initWithString:text attributes:attrs];
+    [[self.textView textStorage] appendAttributedString:attr];
+    [self.textView scrollRangeToVisible:NSMakeRange(self.textView.string.length, 0)];
+    [self.textView setNeedsDisplay:YES];
+}
+
+- (void)clear {
+    self.textView.string = @"";
+}
+
+- (instancetype)initWithFrame:(NSRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        NSScrollView* scrollView = [NSTextView scrollableTextView];
+        scrollView.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+
+        self.textView = scrollView.documentView;
+        self.textView.textContainerInset = NSMakeSize(8, 8);
+        self.textView.backgroundColor = [NSColor colorWithWhite:0.1 alpha:1.0];
+        [self.textView setTextColor:[NSColor whiteColor]];
+        [self.textView setEditable:NO];
+        [self addSubview:scrollView];
+        [self append:@"Waiting...\n"];
+    }
+    return self;
+}
+@end
+
+
 @interface ContentView : NSViewWithTopLeftCoordinateSystem
+@property (retain) LogsView* logsView;
+@property (retain) NSTask* _Nullable ongoingTask;
+@property (atomic, retain) NSMutableString* bufferedData;
+- (instancetype)initWithFrame:(NSRect)frame logsView:(LogsView*)logsView;
 @end
 
 @implementation ContentView
-- (instancetype)initWithFrame:(NSRect)frame {
+- (instancetype)initWithFrame:(NSRect)frame logsView:(LogsView*)logsView {
     if ((self = [super initWithFrame:frame])) {
+        self.logsView = logsView;
+        self.bufferedData = [NSMutableString new];
         NSString* imagePath = @"/Users/philliptennen/Documents/Jailbreak/gala/assets/boot_logo_for_gui.png";
         NSImage* image = [[NSImage alloc] initByReferencingFile:imagePath];
         NSLog(@"image %@", image);
@@ -69,16 +120,14 @@
     return YES;
 }
 
-- (instancetype)initWithFrame:(NSRect)frame {
-    if ((self = [super initWithFrame:frame])) {
-        NSTextView* inner = [[NSTextView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
-        inner.backgroundColor = [NSColor blackColor];
-        [inner setTextColor:[NSColor whiteColor]];
-        [inner setEditable:NO];
-        [inner insertText:@"This is a test!"];
-        [self addSubview:inner];
-    }
-    return self;
+- (void)flushAvailableOutput {
+    [self.logsView append:self.bufferedData];
+    self.bufferedData = [NSMutableString new];
+
+    CGFloat refreshInterval = 0.2;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, refreshInterval * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        [self flushAvailableOutput];
+    });
 }
 @end
 
@@ -97,62 +146,36 @@
                                             |NSWindowStyleMaskMiniaturizable;
         self.window = [[NSWindow alloc] initWithContentRect:windowFrame styleMask:windowStyleMask backing:NSBackingStoreBuffered defer:NO];
         [self.window center];
-        self.window.title = @"gala - A4 tethered iOS 4 jailbreak";
+        self.window.title = @"gala A4 tethered iOS 4 jailbreak";
 
         NSViewWithTopLeftCoordinateSystem* container = [[NSViewWithTopLeftCoordinateSystem alloc] initWithFrame:windowFrame];
-        ContentView* controlsView = [[ContentView alloc] initWithFrame:NSMakeRect(
-            0,
-            0,
-            windowFrame.size.width,
-            windowFrame.size.height * 0.6
-        )];
-        [container addSubview:controlsView];
+        NSRect controlsViewFrame = NSMakeRect(
+                0,
+                0,
+                windowFrame.size.width,
+                windowFrame.size.height * 0.6
+        );
 
         LogsView* logsView = [[LogsView alloc] initWithFrame:NSMakeRect(
             windowFrame.origin.x,
-            //CGRectGetMaxY(controlsView.frame),
-                CGRectGetHeight(controlsView.frame),
+            CGRectGetHeight(controlsViewFrame),
             windowFrame.size.width,
-            CGRectGetHeight(windowFrame) - CGRectGetHeight(controlsView.frame)
+            CGRectGetHeight(windowFrame) - CGRectGetHeight(controlsViewFrame)
         )];
         [container addSubview:logsView];
-        NSLog(@"logs view frame %@", NSStringFromRect(logsView.frame));
+
+        ContentView* controlsView = [[ContentView alloc] initWithFrame:controlsViewFrame logsView:logsView];
+        [container addSubview:controlsView];
 
         self.window.contentView = container;
         self.window.delegate = self;
-        //[self.window makeKeyAndOrderFront:nil];
         [self.window makeKeyWindow];
         [NSApp activateIgnoringOtherApps:YES];
-        //[self.window setOrderedIndex:0];
         [self.window orderFrontRegardless];
         [self.window makeFirstResponder:nil];
     }
     return self;
 }
-
-/*
-- (void)windowDidBecomeKey:(NSNotification *)notification {
-    NSLog(@"Window did become key!");
-}
-
-- (void)windowDidUpdate:(NSNotification *)notification {
-    NSLog(@"Window did update!");
-}
-
-- (void)windowDidResize:(NSNotification *)notification {
-    NSLog(@"resize");
-}
-
-- (void)windowDidMiniaturize:(NSNotification *)notification {
-    NSLog(@"mini");
-}
-
-- (void)windowDidMove:(NSNotification *)notification{NSLog(@"move");}
-- (void)windowDidChangeScreen:(NSNotification *)notification{NSLog(@"changscreen");}
-- (void)windowDidBecomeMain:(NSNotification *)notification{NSLog(@"didBecomeMain");}
-- (void)windowDidExpose:(NSNotification *)notification{NSLog(@"didexpose");}
-- (void)windowDidChangeOcclusionState:(NSNotification *)notification{NSLog(@"didChangeOcc");}
- */
 
 @end
 
