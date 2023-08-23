4b1bae00f5dd6170931db49deff15b83c1624f9e
Phillip Tennen <pt@datatheorem.io>
2023-08-23 10:06:16 +0100

    umount implementation checks the unmount() return code

--- umount/umount.m
+++ umount/umount.m
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <string.h>
+#include <errno.h>
 
 int unmount(const char* path, int flags);
 int printf(const char* fmt, ...);
@@ -6,5 +8,9 @@ int printf(const char* fmt, ...);
 int main(int argc, const char** argv) {
 	const char* path = argv[1];
 	printf("Unmounting %s...\n", path);
-	return unmount(path, 0);
+    int8_t ret = unmount(path, 0);
+    if (ret == -1) {
+        printf("Unmount failed! errnno = %s\n", strerror(errno));
+    }
+    return ret;
 }
