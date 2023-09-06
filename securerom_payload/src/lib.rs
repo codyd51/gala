c95cdd38622fee698a5c1e1bd297491763792c13
Phillip Tennen <pt@datatheorem.io>
2023-09-06 23:04:16 +0100

    Check in Rust-based SecureROM payload

--- securerom_payload/src/lib.rs
+++ securerom_payload/src/lib.rs
@@ -1,52 +1,82 @@
 #![no_std]
-//#![feature(start)]
 #![feature(lang_items)]
 use core;
 use core::panic::PanicInfo;
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
+// PT: Make sure that the Rust entry point is the first defined function, as it's helpful
+// for it to sit right at the start of __text.
 
+const USB_RECV_REGION_BASE: usize = 0x84000000;
+const USB_RECV_REGION_SIZE: usize = 0x2c000;
 
-fn write_str(
-    a: &[u32],
-    b: &mut usize,
-    c: &str,
-    ) {
-    //println!("b {b:?} c {c:?}");
-}
+// Note that each function pointer here has + 1 added so that branches to these addresses switch to THUMB if necessary
+const FUNC_ADDR_NOR_POWER_ON: usize = 0x4e8d;
+const FUNC_ADDR_NOR_INIT: usize = 0x690d;
+const FUNC_ADDR_FREE: usize = 0x3b95;
+const FUNC_ADDR_GET_DFU_IMAGE: usize = 0x4c85;
 
-fn write_u32(
-    a: &[u32],
-    b: &mut usize,
-    c: u32,
-    ) {
-    //println!("b {b:?} c {c:?}");
-}
-
-//#[start]
-//#[allow(unreachable_code)]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-//fn main() -> () {
-    let pc = 0_u32;
-    let sp = 1_u32;
-    let ret = 2_u32;
-    let communication_area = unsafe { core::slice::from_raw_parts_mut(0x84000000 as *mut _, 1024) };
-    communication_area[0] = pc;
-    communication_area[1] = sp;
-    let mut cursor = 0;
-    write_str(
-        communication_area,
-        &mut cursor,
-        "Output from image3_decrypt_payload: ",
-    );
-    write_u32(
-        communication_area,
-        &mut cursor,
-        ret
-        );
-
+#[no_mangle] pub unsafe extern "C"
+//fn await_image() -> i32 {
+fn c_entry_point() -> i32 {
+    //call_func3_at_addr(FUNC_ADDR_NOR_POWER_ON, 1, 1, 0);
+    //call_func1_at_addr(FUNC_ADDR_NOR_INIT, 0);
     0
 }
+
+#[no_mangle] pub unsafe extern "C"
+fn get_image() -> usize {
+    let image_info = call_func2_at_addr(FUNC_ADDR_GET_DFU_IMAGE, USB_RECV_REGION_BASE, USB_RECV_REGION_SIZE);
+
+    let leaking_dfu_buffer_ptr = 0x8402dbcc as *mut usize;
+    let leaking_dfu_buffer_val = *leaking_dfu_buffer_ptr;
+    *leaking_dfu_buffer_ptr = 0;
+    call_func1_at_addr(FUNC_ADDR_FREE, leaking_dfu_buffer_val);
+
+    return image_info;
+}
+
+// Everything that follows is extremely unsafe
+//
+// PT: There's a way to DRY this with macros, but I had trouble getting a proc-macro to build
+// with the armv7-apple-ios toolchain
+
+unsafe fn func0_from_addr(addr: usize) -> extern "C" fn() -> usize {
+    core::mem::transmute(addr as *const ())
+}
+
+unsafe fn call_func0_at_addr(addr: usize) -> usize {
+    let f = func0_from_addr(addr);
+    f()
+}
+
+unsafe fn func1_from_addr(addr: usize) -> extern "C" fn(usize) -> usize {
+    core::mem::transmute(addr as *const ())
+}
+
+unsafe fn call_func1_at_addr(addr: usize, arg0: usize) -> usize {
+    let f = func1_from_addr(addr);
+    f(arg0)
+}
+
+unsafe fn func2_from_addr(addr: usize) -> extern "C" fn(usize, usize) -> usize {
+    core::mem::transmute(addr as *const ())
+}
+
+unsafe fn call_func2_at_addr(addr: usize, arg0: usize, arg1: usize) -> usize {
+    let f = func2_from_addr(addr);
+    f(arg0, arg1)
+}
+
+unsafe fn func3_from_addr(addr: usize) -> extern "C" fn(usize, usize, usize) -> usize {
+    core::mem::transmute(addr as *const ())
+}
+
+unsafe fn call_func3_at_addr(addr: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let f = func3_from_addr(addr);
+    f(arg0, arg1, arg2)
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
