3c493c62ce64317c866f598e23e66f5b86254371
Phillip Tennen <pt@datatheorem.io>
2023-09-08 10:21:50 +0100

    Drop R&D shellcode programs

--- kmem_test/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xcrun -sdk /Users/philliptennen/Documents/Jailbreak/iPhoneSDK4_0.pkg.unzipped/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk clang -arch armv7 -mios-version-min=4.0 kmem_test.m -o build/kmem_test -L./ -framework CoreFoundation
-sshpass -p "alpine" scp -P 2222 -o StrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-dss ./build/kmem_test root@localhost:/kmem_test
-#ssh -oHostKeyAlgorithms=+ssh-dss root@localhost -p 2222
