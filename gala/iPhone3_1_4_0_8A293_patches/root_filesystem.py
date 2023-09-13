73fc1893a5dc6cabcfa76fa79a9bff8a7d89b2e1
Phillip Tennen <pt@datatheorem.io>
2023-09-13 15:41:21 +0100

    Replace vendored GlobalSign cert with a TrustStore that already trusts it

--- gala/iPhone3_1_4_0_8A293_patches/root_filesystem.py
+++ gala/iPhone3_1_4_0_8A293_patches/root_filesystem.py
@@ -24,14 +24,7 @@ def get_rootfs_patches(config: GalaConfig) -> list[DmgPatchSet]:
 
     install_cydia = DmgApplyTarPatch(tar_path=ASSETS_ROOT / "Cydia.tar")
 
-    # Provide the GlobalSign Root G3 certificate, which the user will
-    # need to install to be able to connect to Cydia servers
-    provide_globalsign_root_r3_cert = DmgReplaceFileContentsPatch(
-        file_path=Path("private/var/gala/GlobalSign_Root_R3.crt"),
-        new_content=(ASSETS_ROOT / "GlobalSign_Root_R3.crt").read_bytes(),
-    )
-
-    # Also provide our patched MobileSubstrate build
+    # Provide our patched MobileSubstrate build
     patcher_config = config.patcher_config
     patched_mobile_substrate_name = (
         f"{patcher_config.os_build.asset_path_for_image_type(ImageType.MobileSubstrate).stem}.patched"
@@ -41,14 +34,16 @@ def get_rootfs_patches(config: GalaConfig) -> list[DmgPatchSet]:
         # TODO(PT): Perhaps the IpswPatcherConfig can provide the patched images dir?
         new_content=(patcher_config.patched_images_root() / patched_mobile_substrate_name).read_bytes(),
     )
-
-    provide_deb_for_access_local_files_in_safari = DmgReplaceFileContentsPatch(
-        file_path=Path("private/var/gala/com.bigboss.patchsafari2_2.1.3248-1_iphoneos-arm.deb"),
-        new_content=(ASSETS_ROOT / "com.bigboss.patchsafari2_2.1.3248-1_iphoneos-arm.deb").read_bytes(),
-    )
     provide_deb_for_substrate_safe_mode = DmgReplaceFileContentsPatch(
         file_path=Path("private/var/gala/com.saurik.substrate.safemode_0.9.5000_iphoneos-arm.deb"),
         new_content=(ASSETS_ROOT / "com.saurik.substrate.safemode_0.9.5000_iphoneos-arm.deb").read_bytes(),
+    )
+
+    # This file needs to be owned by the _securityd user.
+    # TODO(PT): Add a validation to ensure the path never starts with /
+    trust_store_trusts_globalsign_root_r3 = DmgReplaceFileContentsPatch(
+        file_path=Path("private/var/Keychains/TrustStore.sqlite3"),
+        new_content=(ASSETS_ROOT / "TrustStore.sqlite3").read_bytes(),
     )
 
     install_dropbear = DmgApplyTarPatch(tar_path=ASSETS_ROOT / "ssh_for_rootfs.tar")
@@ -59,9 +54,8 @@ def get_rootfs_patches(config: GalaConfig) -> list[DmgPatchSet]:
         install_cydia,
         # Delete the Compass app to make room for the Cydia patch
         # DmgRemoveTreePatch(tree_path=Path("Applications/Compass.app")),
-        provide_globalsign_root_r3_cert,
         provide_patched_mobile_substrate,
         provide_deb_for_substrate_safe_mode,
-        provide_deb_for_access_local_files_in_safari,
+        trust_store_trusts_globalsign_root_r3,
     ]
     return [DmgPatchSet(patches=patches)]
