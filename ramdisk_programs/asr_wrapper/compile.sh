98769ce3a5e8644f1a413244feb916ccd3318e34
Phillip Tennen <pt@datatheorem.io>
2023-10-01 10:17:04 +0100

    Use relative paths to iOS 4 SDK

--- ramdisk_programs/asr_wrapper/compile.sh
+++ ramdisk_programs/asr_wrapper/compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-SDK=/Users/philliptennen/Documents/Jailbreak/iPhoneSDK4_0.pkg.unzipped/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk
+SDK=../../iPhoneSDK4_0/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk
 xcrun -sdk $SDK clang -arch armv7 -mios-version-min=4.0 asr_wrapper.m -o build/asr_wrapper -L./ -F$SDK/System/Library/PrivateFrameworks/ -framework CoreFoundation -framework IOKit -framework IOMobileFramebuffer -framework CoreGraphics -framework IOSurface -framework CoreFoundation -framework CoreSurface -lobjc -framework ImageIO
 sshpass -p "alpine" scp -P 2222 -o StrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-dss ./build/asr_wrapper root@localhost:/usr/bin/asr_wrapper
-#ssh -oHostKeyAlgorithms=+ssh-dss root@localhost -p 2222
--- ramdisk_programs/umount/compile.sh
+++ ramdisk_programs/umount/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xcrun -sdk /Users/philliptennen/Documents/Jailbreak/iPhoneSDK4_0.pkg.unzipped/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk clang -arch armv7 -mios-version-min=4.0 umount.m -o build/umount -L./ -framework CoreFoundation
+xcrun -sdk ../../iPhoneSDK4_0/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk clang -arch armv7 -mios-version-min=4.0 umount.m -o build/umount -L./ -framework CoreFoundation
