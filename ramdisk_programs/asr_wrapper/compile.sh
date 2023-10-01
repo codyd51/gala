d10180e3909f8172bf3bb28b2eb0e6521e3639b5
Phillip Tennen <pt@datatheorem.io>
2023-10-01 11:30:30 +0100

    Use new hosted SDK path

--- ramdisk_programs/asr_wrapper/compile.sh
+++ ramdisk_programs/asr_wrapper/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SDK=../../iPhoneSDK4_0/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk
+SDK="$(pwd)"/../../sdks/iPhone3,1_4.0_8A293/iPhoneOS4.0.sdk
 xcrun -sdk $SDK clang -arch armv7 -mios-version-min=4.0 asr_wrapper.m -o build/asr_wrapper -L./ -F$SDK/System/Library/PrivateFrameworks/ -framework CoreFoundation -framework IOKit -framework IOMobileFramebuffer -framework CoreGraphics -framework IOSurface -framework CoreFoundation -framework CoreSurface -lobjc -framework ImageIO
 sshpass -p "alpine" scp -P 2222 -o StrictHostKeyChecking=no -oHostKeyAlgorithms=+ssh-dss ./build/asr_wrapper root@localhost:/usr/bin/asr_wrapper
--- ramdisk_programs/umount/compile.sh
+++ ramdisk_programs/umount/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xcrun -sdk ../../iPhoneSDK4_0/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk clang -arch armv7 -mios-version-min=4.0 umount.m -o build/umount -L./ -framework CoreFoundation
+xcrun -sdk "$(pwd)"/../../sdks/iPhone3,1_4.0_8A293/iPhoneOS4.0.sdk clang -arch armv7 -mios-version-min=4.0 umount.m -o build/umount -L./ -framework CoreFoundation
